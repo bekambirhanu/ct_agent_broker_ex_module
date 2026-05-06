fa3040e1621332d6ab250cd9f4c9a1b29b1fc5dc
Bekam Birhanu <becnolight@gmail.com>
2026-05-06 19:10:32 +0300

    REFACTOR | Refactored broker adapter to use device link id

--- broker_exness/adapter.py
+++ broker_exness/adapter.py
@@ -5,25 +5,26 @@ from nlp_parser.schema import TradeOrder
 from .constants import mt5
 
 class ExnessBroker(BaseBroker):
-    def __init__(self, login: str, password: str, server: str):
+    def __init__(self, device_id: str):
         self.bridge_url = Settings.MT5_BRIDGE_URL
-        self._login = login
-        self._password = password
-        self._server = server
-
+        self.device_id = device_id
+        
+        
     async def execute_order(self, order: TradeOrder) -> dict:
         payload = order.model_dump()
         
         action: int = mt5.TRADE_ACTION_DEAL if payload.get('order_type').upper() == "MARKET" else mt5.TRADE_ACTION_PENDING
         
+        payload.update({
+            "action": action,
+            })
+        
         # Additional constants and configurations
         print(payload)
 
-        # Add user-specific MT5 credentials for the bridge
+        # Add user-specific MT5 credential for the bridge
         payload.update({
-            "login": self._login,
-            "password": self._password,
-            "server": self._server
+            "device_id": self.device_id,
         })
 
         async with httpx.AsyncClient() as client:
